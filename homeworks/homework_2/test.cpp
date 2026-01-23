02f6a049bc8812a2552f3638eaf1eb1799730c4c
ctungyu11 <ctungyu11@gmail.com>
2026-01-23 10:31:28 -0500

    anthony - feat: add edge test cases for string

--- homeworks/homework_2/test.cpp
+++ homeworks/homework_2/test.cpp
@@ -15,6 +15,18 @@ void testStringClass() {
     s3.popBack();
     std::cout << "s3 after popBack: " << s3 << "\n"; // should print "HelloWorld"
 
+    string empty;
+    std::cout << "empty: '" << empty << "' size: " << empty.size() << "\n"; // should print '' size: 0
+    
+    string empty2("");
+    std::cout << "empty2: '" << empty2 << "' size: " << empty2.size() << "\n"; // should print '' size: 0
+
+    s3 += empty;
+    std::cout << "s3 after appending empty: " << s3 << "\n"; // should print "HelloWorld"
+
+    s3 = s1; // assignment operator
+    std::cout << "s3 after assignment: " << s3 << "\n"; // should print "Hello"
+
     string a1("1234");
     string a2("1235");
 
