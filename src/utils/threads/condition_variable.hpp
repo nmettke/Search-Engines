7864dde64ae0cdca4912876668996a861c3ddc97
nmettke <mettke@umich.edu>
2026-03-04 20:34:12 -0500

    added wrappers around pthread mutex, lock_guard, and cv

---
+++ src/utils/threads/condition_variable.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <pthread.h>
+
+class condition_variable
+{
+private:
+    pthread_cond_t cond_;
+
+public:
+    condition_variable()
+    {
+        pthread_cond_init(&cond_, nullptr);
+    }
+
+    void wait(pthread_mutex_t *m)
+    {
+        pthread_cond_wait(&cond_, m);
+    }
+
+    void notify_one()
+    {
+        pthread_cond_signal(&cond_);
+    }
+
+    void notify_all()
+    {
+        pthread_cond_broadcast(&cond_);
+    }
+
+    ~condition_variable()
+    {
+        pthread_cond_destroy(&cond_);
+    }
+};
