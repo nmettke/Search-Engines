54a627c89cdc5fb2bb64cf3f402dd0fbd5bb8db6
nmettke <mettke@umich.edu>
2026-03-13 16:57:20 -0400

    Starter files for frontier class

---
+++ src/crawler/frontier.cpp
@@ -0,0 +1,11 @@
+#include "frontier.h"
+
+bool Frontier::push(const string &url) {}
+
+std::optional<string> Frontier::pop() {}
+
+bool Frontier::contains(const string &url) const {}
+
+std::size_t Frontier::size() const {}
+
+bool Frontier::empty() const {}
--- src/crawler/frontier.h
+++ src/crawler/frontier.h
@@ -1,1 +1,26 @@
 #pragma once
+#include "string.hpp"
+#include "vector.hpp"
+#include <fstream>
+#include <iostream>
+#include <optional>
+
+class Frontier {
+  public:
+    Frontier() {};
+
+    ~Frontier() = default;
+
+    bool push(const string &url);
+
+    std::optional<string> pop();
+
+    bool contains(const string &url) const;
+
+    std::size_t size() const;
+
+    bool empty() const;
+
+  private:
+    vector<string> frontier;
+};
