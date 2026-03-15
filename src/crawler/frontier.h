fb18f6b4d422e091a3829f989ce68c2b59d2a71e
nmettke <123045839+nmettke@users.noreply.github.com>
2026-03-15 16:23:30 -0400

    Frontier (#29)
    * Starter files for frontier class
    
    * Finished writing frontier class
    
    * Fixed clang-format issue

--- src/crawler/frontier.h
+++ src/crawler/frontier.h
@@ -1,1 +1,31 @@
 #pragma once
+#include "condition_variable.hpp"
+#include "lock_guard.hpp"
+#include "mutex.hpp"
+#include "string.hpp"
+#include "vector.hpp"
+#include <fstream>
+#include <iostream>
+#include <optional>
+#include <queue>
+#include <stdexcept>
+class Frontier {
+  public:
+    Frontier(const std::string seed_list_str);
+
+    ~Frontier() = default;
+
+    void push(const string &url);
+
+    std::optional<string> pop();
+
+    bool contains(const string &url) const;
+
+    std::size_t size() const;
+
+    bool empty() const;
+
+  private:
+    std::priority_queue<string> pq;
+    mutex m;
+};
