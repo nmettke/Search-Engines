2883f8f82938e271524aaf832849a3cf68d08bc9
Andrew (Yao-an) Yang <a.yang353188@gmail.com>
2026-03-25 14:59:05 -0400

    in memory index stuff, needs testing

--- src/index/in_memory_index.h
+++ src/index/in_memory_index.h
@@ -1,1 +1,23 @@
 // This header includes code needed to build an index in-memory
+#pragma once
+
+#include "types.h"
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+
+class InMemoryIndex {
+  public:
+    void addToken(const TokenOutput &token);
+    void finishDocument(const DocEndOutput &doc_end);
+
+    const std::unordered_map<std::string, PostingList> &postings() const { return _index; }
+    const std::vector<DocumentRecord> &documents() const { return _documents; }
+    uint64_t totalLocations() const { return _total_locations; }
+
+  private:
+    std::unordered_map<std::string, PostingList> _index;
+    std::vector<DocumentRecord> _documents;
+    std::unordered_set<std::string> _terms_seen;
+    uint64_t _total_locations = 0;
+};
