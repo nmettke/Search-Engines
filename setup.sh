f0a45fc734f3d0b6d39bcc13c3476598ada4a310
Andrew (Yao-an) Yang <a.yang353188@gmail.com>
2026-04-13 04:43:55 -0400

    shell script

---
+++ setup.sh
@@ -0,0 +1,24 @@
+delete_data=false
+
+while getopts "d" opt; do
+    case "$opt" in
+        d) delete_data=true ;;
+    esac
+done
+
+if [ "$delete_data" = true ]; then
+    rm -rf data
+    rm src/crawler/*.dat
+fi
+
+mkdir data
+mkdir data/body_index
+mkdir data/anchor_index
+
+source .env
+
+mkdir build 
+cd build
+cmake ..
+cd ../
+cmake --build build
