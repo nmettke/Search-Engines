69458d6991678ec721a5ba7326fd70f123c0fae1
Satvik  Kapoor <satvik@usegander.com>
2026-04-16 21:59:11 -0400

    changed static ranking config

--- src/index/src/lib/static_rank.h
+++ src/index/src/lib/static_rank.h
@@ -5,16 +5,16 @@
 #include <cstdint>
 
 struct StaticRankConfig {
-    double seed_distance_weight = 2.40;
-    double seed_distance_scale = 1.50;
+    double seed_distance_weight = 3.30;
+    double seed_distance_scale = 1.00;
 
-    double word_count_weight = 1.50;
-    double word_count_pivot = 350.0;
-    double title_word_count_weight = 0.70;
+    double word_count_weight = 0.90;
+    double word_count_pivot = 500.0;
+    double title_word_count_weight = 0.45;
     double title_word_count_pivot = 6.0;
 
-    uint32_t short_doc_threshold = 80;
-    double short_doc_penalty = 0.60;
+    uint32_t short_doc_threshold = 45;
+    double short_doc_penalty = 0.20;
     uint32_t very_long_doc_threshold = 5000;
     double very_long_doc_penalty = 0.20;
 
@@ -24,26 +24,26 @@ struct StaticRankConfig {
     double truncated_penalty = 0.75;
     double open_discard_penalty = 0.50;
 
-    double url_length_weight = 0.45;
-    double url_length_scale = 80.0;
-    double path_length_weight = 0.70;
-    double path_length_scale = 30.0;
-    double path_depth_weight = 0.70;
-    double path_depth_scale = 2.0;
-    double query_param_weight = 0.55;
-    double query_param_scale = 1.0;
-    double numeric_path_weight = 0.45;
-    double numeric_path_scale = 2.0;
-    double domain_hyphen_weight = 0.35;
+    double url_length_weight = 0.55;
+    double url_length_scale = 60.0;
+    double path_length_weight = 0.95;
+    double path_length_scale = 20.0;
+    double path_depth_weight = 1.15;
+    double path_depth_scale = 1.20;
+    double query_param_weight = 0.95;
+    double query_param_scale = 0.80;
+    double numeric_path_weight = 0.75;
+    double numeric_path_scale = 1.00;
+    double domain_hyphen_weight = 0.45;
     double domain_hyphen_scale = 1.0;
 
-    double base_domain_bonus = 0.20;
-    uint8_t min_base_domain_length = 4;
+    double base_domain_bonus = 0.35;
+    uint8_t min_base_domain_length = 3;
     uint8_t max_base_domain_length = 20;
 
-    double outgoing_link_weight = 0.50;
+    double outgoing_link_weight = 0.30;
     double outgoing_link_pivot = 20.0;
-    double outgoing_anchor_weight = 0.40;
+    double outgoing_anchor_weight = 0.20;
     double outgoing_anchor_pivot = 80.0;
 
     double gov_tld_bonus = 0.35;
