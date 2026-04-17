4e90bfb507a54b5550b0e4d0e656d7b47100ef5e
Andrew (Yao-an) Yang <a.yang353188@gmail.com>
2026-04-17 01:42:28 -0400

    More param edits

--- src/main_distributed.cpp
+++ src/main_distributed.cpp
@@ -114,7 +114,7 @@ const string anchorIndexDirectory("data/anchor_index");
 const string indexDirectory("data/body_index");
 const string metaDirectory("data/meta");
 const size_t FLUSHBODYTOKENSIZE = 25000000;
-static constexpr size_t maxIndexQueueItems = 1024;
+static constexpr size_t maxIndexQueueItems = 102400;
 static constexpr size_t crawlerThreadsPerCore = 512;
 static constexpr size_t fallbackCrawlerThreadCount = 8;
 static constexpr size_t maxCrawlerThreadCount = 2400;
